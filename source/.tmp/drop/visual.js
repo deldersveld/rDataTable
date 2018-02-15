8280444393b2b9a0f2772f13bce813bc0d1ef2ce
David Eldersveld <deldersveld@blue-granite.com>
2018-02-15 08:28:58 -0500

    Version 1.1.0.2

--- source/.tmp/drop/visual.js
+++ source/.tmp/drop/visual.js
@@ -964,7 +964,7 @@ var powerbi;
                 name: 'rDataTableF01A81A1541448719C37BCD0F9EABDD7',
                 displayName: 'rDataTable',
                 class: 'Visual',
-                version: '1.1.0.1',
+                version: '1.1.0.2',
                 apiVersion: '1.7.0',
                 create: function (options) { return new powerbi.extensibility.visual.rDataTableF01A81A1541448719C37BCD0F9EABDD7.Visual(options); },
                 custom: true
